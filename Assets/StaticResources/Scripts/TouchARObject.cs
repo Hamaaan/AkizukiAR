5640658d927237033df1aab1e6eb59cb7087a30b
Hamaaan <51164087+Hamaaan@users.noreply.github.com>
2023-05-15 08:14:26 +0900

    h

--- Assets/StaticResources/Scripts/TouchARObject.cs
+++ Assets/StaticResources/Scripts/TouchARObject.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 public class TouchARObject : MonoBehaviour
 {
+    public TrackableType type;
+
+    ARRaycastManager raycastManager;
+    List<ARRaycastHit> hitResults = new List<ARRaycastHit>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        raycastManager = GetComponent<ARRaycastManager>();
     }
 
     // Update is called once per frame
@@ -21,6 +28,13 @@ public class TouchARObject : MonoBehaviour
             {
                 hit.collider.gameObject.SendMessage("GetTouch");
             }
+
+            if (raycastManager.Raycast(Input.GetTouch(0).position, hitResults, TrackableType.AllTypes))
+            {
+                Debug.Log(hitResults[0].pose.position);
+            }
         }
+
+
     }
 }
